cec661919615338e021f6057ff3d094135f14e64
martinrdrz <martinrdrz@hotmail.com>
2024-06-12 14:04:26 -0300

    finalizado

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
     const dispatch = useDispatch();
+    const { page, isLoading, pokemons = [] } = useSelector((state) => state.pokemons);
 
     useEffect(() => {
         //Se utiliza un dispatch para disparar lo reducers, pero los mismos son sincronos y se necesita disparar
@@ -15,11 +16,15 @@ export const PokemonApp = () => {
         <>
             <h1>PokemonApp</h1>
             <hr />
-            <ul>
-                <li>hola</li>
-                <li>hola</li>
-                <li>hola</li>
+            <span>Loading:{isLoading ? 'True' : 'False'}</span>
+            <ul style={{ textAlign: 'left', listStylePosition: 'inside', paddingLeft: 0 }}>
+                {pokemons.map(({ name }) => (
+                    <li key={name}>{name}</li>
+                ))}
             </ul>
+            <button disable={isLoading.toString()} onClick={() => dispatch(getPokemons(page))}>
+                Next
+            </button>
         </>
     );
 };
